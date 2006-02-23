f94896960b190d1c2130a68a87caeec999b4a9d5
pmatilai <pmatilai>
2006-02-23 18:39:15 +0000

    - Make used compression method a feature of a given repository type
    (cherry picked from commit d5d8942bc9e99bb42977765f3db03c547395da26)

--- apt-pkg/repository.h
+++ apt-pkg/repository.h
@@ -24,6 +24,7 @@ class pkgRepository
    map<string,Checksum> IndexChecksums; // path -> checksum data
 
    bool GotRelease;
+   string ComprMethod;
 
    public:
 
@@ -34,12 +35,14 @@ class pkgRepository
 
    bool Acquire;
 
-   // PM 2006-21-02 make these methods virtual
+   // LORG:2006-02-21 make these methods virtual
    virtual bool ParseRelease(string File);
    virtual bool HasRelease() const { return GotRelease; }
 
    virtual bool IsAuthenticated() const { return !FingerPrint.empty(); };
    virtual bool FindChecksums(string URI,unsigned long &Size, string &MD5);
+   // LORG:2006-02-23
+   virtual string GetComprMethod() {return ComprMethod;};
 
    pkgRepository(string URI,string Dist, const pkgSourceList::Vendor *Vendor,
 		 string RootURI)
@@ -47,7 +50,8 @@ class pkgRepository
 	Acquire(1)
    {
       if (Vendor) FingerPrint = Vendor->FingerPrint;
-   }
+      ComprMethod = "bz2";
+   };
 
 };
 
