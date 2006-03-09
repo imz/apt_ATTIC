4dc32d724a3f76cfe9d126bfeb689a773ec649f7
pmatilai <pmatilai>
2006-03-09 20:56:51 +0000

    - working shasum verification for repomd .. alas it breaks traditional   repos at the moment :(
    (cherry picked from commit b4095f5c9e7e906ce409d189c6f138994884a312)
    
    - allow per-repository-type checksum method (sha/md5)
    
    (cherry picked from commit a7b0215d39cbfc61c0af0bb261aa8acf8c0d33f6)

--- apt-pkg/repository.h
+++ apt-pkg/repository.h
@@ -25,6 +25,7 @@ class pkgRepository
 
    bool GotRelease;
    string ComprMethod;
+   string CheckMethod;
 
    public:
 
@@ -43,6 +44,7 @@ class pkgRepository
    virtual bool FindChecksums(string URI,unsigned long &Size, string &MD5);
    // LORG:2006-02-23
    virtual string GetComprMethod() {return ComprMethod;};
+   virtual string GetCheckMethod() {return CheckMethod;};
 
    pkgRepository(string URI,string Dist, const pkgSourceList::Vendor *Vendor,
 		 string RootURI)
@@ -51,7 +53,8 @@ class pkgRepository
    {
       if (Vendor) FingerPrintList = Vendor->FingerPrintList;
       ComprMethod = "bz2";
-   }
+      CheckMethod = "MD5-Hash";
+   };
 
 };
 
