a4015a68b90597ee0b34270b2b29fb1823c910e3
Panu Matilainen <pmatilai@laiskiainen.org>
2008-02-21 15:29:37 +0200

    Remove bogus semicolons in inline method definitions (Bernhard Rosenkränzer) [repository.h]
    [imz: The part of the change that is actual after the backported work only
    on cksum types for pkglist indices.]
    
    (Originally 17c28a2fe288a7d6508425ac3e3b0a4baf0c94fa at http://apt-rpm.org/scm/apt.git )
    
    (Originally d16036df4a3f375a0621f8c3391cda8381d9ad44 at https://gitlab.com/apt-rpm/apt-rpm.git )
    
    A commit selected with:
    
    git log --oneline --reverse 0.5.15sisyphus1/apt-repomd..0.5.15sisyphus1/apt-rpm --regexp-ignore-case -G'hash|md5|sha|cksum|checksum|chktype'

--- apt-pkg/repository.h
+++ apt-pkg/repository.h
@@ -42,8 +42,8 @@ class pkgRepository
    virtual bool IsAuthenticated() const { return !FingerPrintList.empty(); }
    virtual bool FindChecksums(string URI,unsigned long &Size, string &MD5);
    // LORG:2006-02-23
-   virtual string GetCheckMethod() {return CheckMethod;};
-   virtual string GetComprMethod(string URI) {return "bz2";};
+   virtual string GetCheckMethod() {return CheckMethod;}
+   virtual string GetComprMethod(string URI) {return "bz2";}
 
    pkgRepository(string URI,string Dist, const pkgSourceList::Vendor *Vendor,
 		 string RootURI)
@@ -52,7 +52,7 @@ class pkgRepository
    {
       if (Vendor) FingerPrintList = Vendor->FingerPrintList;
       CheckMethod = "MD5-Hash";
-   };
+   }
 
 };
 
