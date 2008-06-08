77871e5c0331ac9c68c4db09faab1a09a4e1857e
Panu Matilainen <pmatilai@redhat.com>
2008-06-08 09:15:26 +0300

    Add new private header to hide differences in rpm types from apt
    - while C doesn't mind converting enums to ints etc automatically, C++
      gets very upset, use raptFoo types to hide these
    
    (cherry picked from commit 9b8e667b1fd1b4470ed8a576ed417a36a5f81906)

---
+++ apt-pkg/rpm/rapttypes.h
@@ -0,0 +1,18 @@
+#ifndef PKGLIB_RAPTTYPES_H
+#define PKGLIB_RAPTTYPES_H
+
+/*
+ * Layer of insulation against differing types used in rpm versions.
+ * C happily converts enum to int etc automatically, C++ doesn't...
+ */
+
+#include <rpm/rpmtypes.h>
+#include <rpm/rpmds.h>
+typedef rpm_tag_t raptTag;
+typedef rpm_tagtype_t raptTagType;
+typedef rpmsenseFlags raptDepFlags;
+typedef uint32_t raptInt;
+typedef uint32_t raptDbOffset;
+#define raptInitIterator(a,b,c,d) rpmtsInitIterator(a,(rpmTag)b,c,d)
+
+#endif /* PKGLIB_RAPTTYPES_H */
