00285c5b3ac69c7cbd3c4d6efd853d561ae694bc
Panu Matilainen <pmatilai@redhat.com>
2008-12-13 18:47:51 +0200

    Add beginnings of C++ level abstraction for rpm header
    - bury rpm version differences one level deeper and let rest of apt
      remain ignorant what kludgery is needed to pull out given type of
      data out of headers
    - converts the "raw" C arrays and strings to C++ vectors and such for nicer
      handling
    
    (cherry picked from commit b3ecfd7ec10b88ce5e45cf6a578e71dda378d892)
    
    headerGet() flag fiddles
    
    (cherry picked from commit 30c7bf31a4b6e11e1e36f1096075c6caf8fd8786)
    
    Non-array cases of getTag() don't need rpm-version specific implementation
    
    (cherry picked from commit 88a36aa91a7e2e1c82f599f12e50e10bac132aa7)
    
    More rpm version compatibility fiddling in raptheader
    
    (cherry picked from commit e0573d5466f9357bda6bdfc28e2c83cb17ab3aa5)
    
    Add format() method to raptheader to wrap headerSprintf() / headerFormat()
    
    (cherry picked from commit e2cbe3d591dbdabd845e246fc71e9e2fcb917f30)
    
    headerUnlink() is wrong, use headerFree() instead
    
    (cherry picked from commit dfcec4ea45210b5d83ab65ecadb35001c7d39913)
    
    Constify raptHeader getter methods
    
    (cherry picked from commit 796c5fff00f83b1ca9f94f0dc7b8eb7f6d480279)

---
+++ apt-pkg/rpm/raptheader.cc
@@ -0,0 +1,111 @@
+#include <config.h>
+#include <iostream>
+
+#include "raptheader.h"
+
+using namespace std;
+
+/*
+ * Beginnings of C++ wrapper for RPM header and data.
+ * Having a rpm version independent ways to access header data makes
+ * things a whole lot saner in other places while permitting newer
+ * rpm interfaces to be used where supported.
+ *
+ * Lots to do still:
+ * - actually implement "raw" string mode + handle i18n strings correctly
+ * - we'll want to eventually replace rpmhandler's HeaderP with raptHeader(),
+ *   add constructors for reading in from files, db iterators & all etc...
+ */
+
+raptHeader::raptHeader(Header h)
+{
+   Hdr = headerLink(h);
+}
+
+raptHeader::~raptHeader()
+{
+   headerFree(Hdr);
+}
+
+bool raptHeader::hasTag(raptTag tag) const
+{
+   return headerIsEntry(Hdr, tag);
+}
+
+// XXX: should have a way to pass back formatting error messages
+string raptHeader::format(const string &fmt) const
+{
+   string res;
+   char *s = headerFormat(Hdr, fmt.c_str(), NULL);
+   if (s) {
+      res = string(s);
+      free(s);
+   }
+   return res;
+}
+
+bool raptHeader::getTag(raptTag tag, raptInt &data) const
+{
+   vector<raptInt> _data;
+   bool ret = false;
+
+   if (getTag(tag, _data) && _data.size() == 1) {
+      data = _data[0];
+      ret = true;
+   }
+   return ret;
+}
+
+bool raptHeader::getTag(raptTag tag, string &data, bool raw) const
+{
+   vector<string> _data;
+   bool ret = false;
+
+   if (getTag(tag, _data, raw) && _data.size() == 1) {
+      data = _data[0];
+      ret = true;
+   }
+   return ret;
+}
+
+// use MINMEM to avoid extra copy from header if possible
+#define HGDFL (headerGetFlags)(HEADERGET_EXT | HEADERGET_MINMEM)
+#define HGRAW (headerGetFlags)(HGDFL | HEADERGET_RAW)
+
+bool raptHeader::getTag(raptTag tag, vector<string> &data, bool raw) const
+{
+   struct rpmtd_s td;
+   bool ret = false;
+   headerGetFlags flags = raw ? HGRAW : HGDFL;
+
+   if (headerGet(Hdr, tag, &td, flags)) {
+      if (rpmtdClass(&td) == RPM_STRING_CLASS) {
+	 const char *str;
+	 while ((str = rpmtdNextString(&td))) {
+	    data.push_back(str);
+	 }
+	 ret = true;
+      }
+      rpmtdFreeData(&td);
+   }
+   return ret;
+}
+
+bool raptHeader::getTag(raptTag tag, vector<raptInt> &data) const
+{
+   struct rpmtd_s td;
+   bool ret = false;
+   if (headerGet(Hdr, tag, &td, HGDFL)) {
+      if (rpmtdType(&td) == RPM_INT32_TYPE) {
+	 raptInt *num;
+	 while ((num = rpmtdNextUint32(&td))) {
+	    data.push_back(*num);
+	 }
+	 ret = true;
+      }
+      rpmtdFreeData(&td);
+   }
+   return ret;
+}
+
+// vim:sts=3:sw=3
