db406987304c9aa3991d2a9e0edaca916c07a3ce
Panu Matilainen <pmatilai@redhat.com>
2008-12-13 18:47:51 +0200

    Add beginnings of C++ level abstraction for rpm header
    - bury rpm version differences one level deeper and let rest of apt
      remain ignorant what kludgery is needed to pull out given type of
      data out of headers
    - converts the "raw" C arrays and strings to C++ vectors and such for nicer
      handling
    
    (cherry picked from commit b3ecfd7ec10b88ce5e45cf6a578e71dda378d892)
    
    headerGet() flag fiddles
    
    (cherry picked from commit 30c7bf31a4b6e11e1e36f1096075c6caf8fd8786)
    
    Non-array cases of getTag() don't need rpm-version specific implementation
    
    (cherry picked from commit 88a36aa91a7e2e1c82f599f12e50e10bac132aa7)
    
    More rpm version compatibility fiddling in raptheader
    
    (cherry picked from commit e0573d5466f9357bda6bdfc28e2c83cb17ab3aa5)
    
    Add format() method to raptheader to wrap headerSprintf() / headerFormat()
    
    (cherry picked from commit e2cbe3d591dbdabd845e246fc71e9e2fcb917f30)
    
    headerUnlink() is wrong, use headerFree() instead
    
    (cherry picked from commit dfcec4ea45210b5d83ab65ecadb35001c7d39913)
    
    Constify raptHeader getter methods
    
    (cherry picked from commit 796c5fff00f83b1ca9f94f0dc7b8eb7f6d480279)

---
+++ apt-pkg/rpm/raptheader.h
@@ -0,0 +1,34 @@
+#ifndef PKGLIB_RAPTHEADER_H
+#define PKGLIB_RAPTHEADER_H
+
+#include <vector>
+#include <string>
+
+#include "rapttypes.h"
+
+#include <rpm/header.h>
+#include <rpm/rpmtag.h>
+
+using std::vector;
+using std::string;
+
+class raptHeader
+{
+   private:
+   Header Hdr;
+
+   public:
+   bool hasTag(raptTag tag) const;
+   bool getTag(raptTag tag, raptInt &data) const;
+   bool getTag(raptTag tag, string &data, bool raw = false) const;
+   bool getTag(raptTag tag, vector<raptInt> &data) const;
+   bool getTag(raptTag tag, vector<string> &data, bool raw = false) const;
+   string format(const string &fmt) const;
+
+   raptHeader(Header hdr);
+   ~raptHeader();
+};
+
+#endif /* _PKGLIB_RAPTHEADER_H */
+
+// vim:sts=3:sw=3
