5f4ba3bc3082cabbb4fa7aee88d86831b7ab53a3
Anton V. Boyarshinov <boyarsh@altlinux.org>
2019-02-01 11:34:43 +0300

    apt-get moo eastern egg removed

--- apt/cmdline/apt-get.cc
+++ apt/cmdline/apt-get.cc
@@ -3151,23 +3151,6 @@ bool DoBuildDep(CommandLine &CmdL)
 }
 									/*}}}*/
 
-// DoMoo - Never Ask, Never Tell					/*{{{*/
-// ---------------------------------------------------------------------
-/* */
-bool DoMoo(CommandLine &CmdL)
-{
-   cout << 
-      "         (__) \n"
-      "         (oo) \n"
-      "   /------\\/ \n"
-      "  / |    ||   \n" 
-      " *  /\\---/\\ \n"
-      "    ~~   ~~   \n"
-      "....\"Have you mooed today?\"...\n";
-			    
-   return true;
-}
-									/*}}}*/
 
 // CNC:2003-03-18
 // DoScript - Scripting stuff.						/*{{{*/
@@ -3388,7 +3371,6 @@ int main(int argc,const char *argv[])
                                    {"autoclean",&DoAutoClean},
                                    {"check",&DoCheck},
 				   {"source",&DoSource},
-				   {"moo",&DoMoo},
 				   {"help",&ShowHelp},
 // CNC:2003-03-19
 #ifdef WITH_LUA
