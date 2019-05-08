8fccbf603b66b56f4f0747069c6647f9e565f547
Ivan Zakharyaschev <imz@altlinux.org>
2019-05-08 04:12:52 +0300

    rpmListParser::Version() rewritten with stringstream (to avoid cumbersome snprintf())

--- apt/apt-pkg/rpm/rpmlistparser.cc
+++ apt/apt-pkg/rpm/rpmlistparser.cc
@@ -184,6 +184,9 @@ string rpmListParser::Architecture()
    return string(res?arch:"");
 }
                                                                         /*}}}*/
+
+#include <sstream>
+
 // ListParser::Version - Return the version string			/*{{{*/
 // ---------------------------------------------------------------------
 /* This is to return the string describing the version in RPM form,
@@ -200,8 +203,7 @@ string rpmListParser::Version()
    int32_t *ser;
    bool has_epoch = false;
    int type, count;
-   string str;
-   str.reserve(10);
+   stringstream ss;
 
    if (headerGetEntry(header, RPMTAG_EPOCH, &type, (void **)&ser, &count) == 1
        && count > 0) 
@@ -210,16 +212,11 @@ string rpmListParser::Version()
    headerGetEntry(header, RPMTAG_VERSION, &type, (void **)&ver, &count);
    headerGetEntry(header, RPMTAG_RELEASE, &type, (void **)&rel, &count);
 
-   if (has_epoch == true) {
-      char buf[32];
-      snprintf(buf, sizeof(buf), "%i", ser[0]);
-      str += buf;
-      str += ":";
+   if (has_epoch) {
+      ss << ser[0] << ":";
    }
-   str += ver;
-   str += "-";
-   str += rel;
-   return str;
+   ss << ver << "-" << rel;
+   return ss.str();
 }
                                                                         /*}}}*/
 // ListParser::NewVersion - Fill in the version structure		/*{{{*/
