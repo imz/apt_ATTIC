d1be12fdc34fa85728156f15e6bc04027b0bcead
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-05-17 13:58:36 +0300

    pkgAcqMethod: add Warning function

--- apt/apt-pkg/acquire-method.cc
+++ apt/apt-pkg/acquire-method.cc
@@ -520,6 +520,30 @@ void pkgAcqMethod::Status(const char *Format,...)
    if (write(STDOUT_FILENO,S.c_str(),S.size()) != (ssize_t)S.size())
       exit(100);
 }
+
+void pkgAcqMethod::Warning(const char *Format,...)
+{
+   string CurrentURI = "<UNKNOWN>";
+   if (Queue != 0)
+      CurrentURI = Queue->Uri;
+
+   va_list args;
+   va_start(args,Format);
+
+   ostringstream s;
+   s << "104 Warning\nURI: " << CurrentURI << "\nMessage: ";
+
+   // sprintf the description
+   char Buf[1024];
+   vsnprintf(Buf,sizeof(Buf)-4,Format,args);
+   va_end(args);
+   s << Buf << "\n\n";
+
+   string S = s.str();
+   if (write(STDOUT_FILENO,S.c_str(),S.size()) != (ssize_t)S.size())
+      exit(100);
+}
+
 									/*}}}*/
 // AcqMethod::Redirect - Send a redirect message			/*{{{*/
 // ---------------------------------------------------------------------
