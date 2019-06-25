afc033da36abbfe16ffea6c07b78f68f3aa6ff73
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-06-25 18:01:55 +0300

    packagekit support: Port pkgCacheFile::GetSourceList and its dependencies from Debian
    Required for improved packagekit support.
    
    Change-Id: Ia98bfa2d5535f7258a328949d19aca1f7f65a4e2
    
    [imz: test/integration/test-apt-config-dump adapted so that the
    pattern matches only things that are close to the real
    Dir::Ignore-Files-Silently elements and nothing else.]

--- apt-pkg/init.cc
+++ apt-pkg/init.cc
@@ -89,6 +89,15 @@ bool pkgInitConfig(Configuration &Cnf)
    Cnf.CndSet("Dir::Bin::methods",LIBDIR "/apt/methods");
    Cnf.CndSet("Acquire::ComprExtension", ".bz2");
 
+   Cnf.Set("Dir::Ignore-Files-Silently::", "~$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.disabled$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.bak$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.dpkg-[a-z]+$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.ucf-[a-z]+$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.save$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.orig$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.distUpgrade$");
+
    bool Res = true;
 
    // Read an alternate config file
