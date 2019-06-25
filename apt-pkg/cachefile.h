2e5036bb276444dad9b6c2c37b7e8a20a40d4773
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-06-25 18:01:55 +0300

    packagekit support: Port pkgCacheFile::GetSourceList and its dependencies from Debian
    Required for improved packagekit support.
    
    Change-Id: Ia98bfa2d5535f7258a328949d19aca1f7f65a4e2
    
    [imz: test/integration/test-apt-config-dump adapted so that the
    pattern matches only things that are close to the real
    Dir::Ignore-Files-Silently elements and nothing else.
    
    Reordered methods in cachefile.h as in Debian.
    ]

--- apt-pkg/cachefile.h
+++ apt-pkg/cachefile.h
@@ -22,6 +22,8 @@
 #include <memory>
 
 class pkgPolicy;
+class pkgSourceList;
+
 class pkgCacheFile
 {
    protected:
@@ -29,6 +31,7 @@ class pkgCacheFile
    MMap *Map;
    pkgCache *Cache;
    pkgDepCache *DCache;
+   pkgSourceList *SrcList;
 
    public:
 
@@ -39,14 +42,22 @@ class pkgCacheFile
    inline operator pkgCache *() const {return Cache;}
    inline operator pkgDepCache &() const {return *DCache;}
    inline operator pkgDepCache *() const {return DCache;}
+   inline operator pkgSourceList &() const {return *SrcList;}
+   inline operator pkgSourceList *() const {return SrcList;}
    inline pkgDepCache *operator ->() const {return DCache;}
    inline pkgDepCache &operator *() const {return *DCache;}
    inline pkgDepCache::StateCache &operator [](pkgCache::PkgIterator const &I) const {return (*DCache)[I];}
    inline unsigned char &operator [](pkgCache::DepIterator const &I) const {return (*DCache)[I];}
 
    bool BuildCaches(OpProgress &Progress,bool WithLock);
+   bool BuildSourceList(OpProgress *Progress = NULL);
    bool Open(OpProgress &Progress,bool WithLock);
+   static void RemoveCaches();
    void Close();
+
+   inline pkgSourceList* GetSourceList() { BuildSourceList(); return SrcList; }
+
+   inline bool IsSrcListBuilt() const { return (SrcList != NULL); }
 
    pkgCacheFile();
    ~pkgCacheFile();
