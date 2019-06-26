39930789a1587f13165d98f2e00496f3f5088e6e
Aleksei Nikiforov <darktemplar@altlinux.org>
2019-06-26 11:47:48 +0300

    Port ListUpdate function from Debian
    Update apt-get and apt-shell to use new function.
    
    [imz: reapply to test packagekit with the traditional APT API. There
    have been no real conflicts: mostly whitespace; left out some hunks
    that changed just the formatting.]
    
    (cherry picked from commit 6f3ab7196cf0bf6daa54240b5bc2d8d11d2c811c)

---
+++ apt-pkg/update.h
@@ -0,0 +1,19 @@
+// -*- mode: cpp; mode: fold -*-
+// Description								/*{{{*/
+/* ######################################################################
+
+   Update - ListUpdate related code
+
+   ##################################################################### */
+									/*}}}*/
+
+#ifndef PKGLIB_UPDATE_H
+#define PKGLIB_UPDATE_H
+
+class pkgAcquireStatus;
+class pkgSourceList;
+class pkgCacheFile;
+
+bool ListUpdate(pkgAcquireStatus &progress, pkgSourceList &List, pkgCacheFile &Cache);
+
+#endif
