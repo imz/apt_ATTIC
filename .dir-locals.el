08c7b7a973f327282a503b90217a4cb5d1de0e3c
Ivan Zakharyaschev <imz@altlinux.org>
2020-02-10 13:03:07 +0300

    .dir-locals.el: improve Emacs customization
    * apply our Emacs mode settings to .h files, too
    * follow this project's style for indenting braces after "if", "for" etc.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
+((nil
+  (c-file-style . "bsd") ;; for indenting braces after "if", "for" etc.
+  (c-basic-offset . 3))
 
-((c++-mode
-  (c-basic-offset . 3)))
+ ;; For switching the mode in .h headers:
+ (c-mode
+  (mode . c++)))
