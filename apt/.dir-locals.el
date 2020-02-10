14da2919e97cad1a0686d1bb4cc08261c9a4eb77
Ivan Zakharyaschev <imz@altlinux.org>
2020-02-10 13:03:07 +0300

    .dir-locals.el: apply our Emacs mode settings to .h files, too

--- apt/.dir-locals.el
+++ apt/.dir-locals.el
@@ -2,4 +2,9 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode
+  (c-basic-offset . 3))
+
+ ;; For switching in .h headers:
+ (c-mode
+  (mode . c++)
   (c-basic-offset . 3)))
