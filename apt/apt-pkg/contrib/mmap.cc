4691d63d00fbe2176ac46c7edf7e34e956340de5
Ivan Zakharyaschev <imz@altlinux.org>
2020-02-10 19:29:34 +0300

    DynamicMMap::WriteString(): fix the check for "out of room" when length is automatic
    It made no sense to make the check before we have the valid value in Len.

--- apt/apt-pkg/contrib/mmap.cc
+++ apt/apt-pkg/contrib/mmap.cc
@@ -271,6 +271,9 @@ unsigned long DynamicMMap::Allocate(unsigned long ItemSize)
 unsigned long DynamicMMap::WriteString(const char *String,
 				       unsigned long Len)
 {
+   if (Len == (unsigned long)-1)
+      Len = strlen(String);
+
    unsigned long Result = iSize;
    // Just in case error check
    if (Result + Len > WorkSpace)
@@ -279,8 +282,6 @@ unsigned long DynamicMMap::WriteString(const char *String,
       return 0;
    }
 
-   if (Len == (unsigned long)-1)
-      Len = strlen(String);
    iSize += Len + 1;
    memcpy((char *)Base + Result,String,Len);
    ((char *)Base)[Result + Len] = 0;
