25b51185821e8c2e43ef56fb6d7c52d9db50a08f
Ivan Zakharyaschev <imz@altlinux.org>
2020-08-06 16:09:02 +0300

    DynamicMMap: add overloaded WriteString() instead of using an ugly default arg

--- apt-pkg/contrib/mmap.h
+++ apt-pkg/contrib/mmap.h
@@ -88,7 +88,8 @@ class DynamicMMap : public MMap
    // Allocation
    unsigned long RawAllocate(unsigned long Size,unsigned long Aln = 0);
    unsigned long Allocate(unsigned long ItemSize);
-   unsigned long WriteString(const char *String,unsigned long Len = (unsigned long)-1);
+   unsigned long WriteString(const char *String,unsigned long Len);
+   unsigned long WriteString(const char *String); // uses strlen()
    inline unsigned long WriteString(string const S) {return WriteString(S.c_str(),S.length());}
    void UsePools(Pool &P,unsigned int const Count) {Pools = &P; PoolCount = Count;}
 
