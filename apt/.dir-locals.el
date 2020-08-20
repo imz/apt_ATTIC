74b73259afa73ea44db0101d1b0ce1663c1dc1ae
Ivan Zakharyaschev <imz@altlinux.org>
2020-08-20 17:38:15 +0300

    .dir-locals.el: don't mix spaces and tabs in new indentation (for Emacs)
    It would be better to have just spaces in the indentation, especially
    given the strange indentation offset in APT sources (3): inserting
    tabs to match the positions 9, 12 etc. looks crazy.

--- apt/.dir-locals.el
+++ apt/.dir-locals.el
@@ -14,9 +14,15 @@
 
   )
 
+ (c++-mode
+  ;; Don't mix tabs/spaces (e.g., when indenting to pos 9):
+  (indent-tabs-mode . nil)
+  )
+
  ;; For switching the mode in .h headers:
  (c-mode
   (mode . c++)
+  (indent-tabs-mode . nil)
   )
 
  )
