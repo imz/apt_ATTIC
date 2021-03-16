a29aef4672f2d8530077b61c108b470ead7b35ed
Ivan Zakharyaschev <imz@altlinux.org>
2021-03-16 22:50:44 +0300

    Revert a 0.5.15lorg2-alt70 std::optional change to be re-based: "Add workaround for packages with missing tags"
    This reverts commit f91e5f298abe9b6ba23237f1098c5266cd28aeb1.

--- apt/apt-pkg/rpm/rpmlistparser.cc
+++ apt/apt-pkg/rpm/rpmlistparser.cc
@@ -83,14 +83,7 @@ std::experimental::optional<unsigned long> rpmListParser::UniqFindTagWrite(int T
    void *data;
    
    if (headerGetEntry(header, Tag, &type, &data, &count) != 1)
-   {
-      /*
-       * Some 3rd-party rpm packages may not contain these tags.
-       * But since cacheiterators treat zero as special value,
-       * just pass it instead of failing
-       */
-      return std::experimental::optional<unsigned long>(0);
-   }
+      return std::experimental::optional<unsigned long>();
    
    if (type == RPM_STRING_TYPE) 
    {
