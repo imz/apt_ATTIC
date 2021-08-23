79b9b55bf435ab9f6843c79b28925197a4ce2057
Ivan Zakharyaschev <imz@altlinux.org>
2021-08-23 21:37:55 +0300

    for testing, hardcode SHA256 as the cksum type for pkglist indices (stored in "release" files)

--- apt-pkg/repository.cc
+++ apt-pkg/repository.cc
@@ -45,12 +45,12 @@ bool pkgRepository::ParseRelease(string File)
 
    GotRelease = true;
 
-   string Files = Section.FindS("MD5Sum");
+   string Files = Section.FindS("SHA256");
    // Lack of checksum is only fatal if authentication is on
    if (Files.empty())
    {
       if (IsAuthenticated())
-	 return _error->Error(_("No MD5Sum data in Release file '%s'"),
+	 return _error->Error(_("No SHA256 data in Release file '%s'"),
 			      Files.c_str());
       else
 	 return true;
@@ -75,7 +75,7 @@ bool pkgRepository::ParseRelease(string File)
                  ParseQuoteWord_(C))
          : std::nullopt;
       if (! Path)
-         return _error->Error(_("Error parsing MD5Sum hash record on Release file '%s'"),
+         return _error->Error(_("Error parsing SHA256 hash record on Release file '%s'"),
                               File.c_str());
       IndexChecksums[*Path].Size = *Size;
       IndexChecksums[*Path].MD5 = *Hash;
