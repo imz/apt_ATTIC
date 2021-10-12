4349566d787109f7f17a4f944b58f23e4b91f23f
Ivan Zakharyaschev <imz@altlinux.org>
2021-10-12 15:21:57 +0300

    Revert "fixup! 3d0bd9350 acquire-item.cc: added support for xz-compressed pkglists"
    This reverts commit e1b9fd500cf8abb4bec078b5af74216a7c6ff92e
    to rebase on top of apt-rpm's code with per-repo-type compression
    (determined by Repository class).

--- apt-pkg/acquire-item.cc
+++ apt-pkg/acquire-item.cc
@@ -250,9 +250,6 @@ pkgAcqIndex::pkgAcqIndex(pkgAcquire *Owner,pkgRepository *Repository,
 	    unlink(FinalFile.c_str());
 	    unlink(DestFile.c_str());
 	 }
-
-	 if (Repository->FindChecksums(RealURI + ".xz", Size, MD5Hash) == true)
-	    Desc.URI = URI + ".xz";
       }
       else if (Repository->IsAuthenticated() == true)
       {
@@ -389,20 +386,16 @@ void pkgAcqIndex::Done(string Message,unsigned long Size, const string /* MD5 ob
    Decompression = true;
    DestFile += ".decomp";
    // LORG:2006-02-23 compression is a feature of repository type
-   // ALT: no, it's simply determined by the extension appended in ctor above.
-   const char *prog = nullptr;
-   const std::string ext = flExtension(Desc.URI);
-   if (ext == "xz")
-      prog = "xz";
-   else if (ext == "gz")
-      prog = "gzip";
-   else if (ext == "bz2")
-      prog = "bzip2";
-   else
+   if (Repository->GetComprMethod() == "gz") {
+      Desc.URI = "gzip:" + FileName;
+      Mode = "gzip";
+   } else if (Repository->GetComprMethod() == "bz2") {
+      Desc.URI = "bzip2:" + FileName;
+      Mode = "bzip2";
+   } else {
       _error->Warning(_("Uknown compression extension, trying uncompressed"));
-   Desc.URI = (prog ? std::string(prog) + ":" : std::string()) + FileName;
-   // CNC:2002-07-03
-   Mode = prog;
+      Desc.URI = FileName;
+   }
    QueueURI(Desc);
 }
 									/*}}}*/
