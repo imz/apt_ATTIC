03049d75aee1cac83d3e12c0c8305548f3ba8fe7
Ivan Zakharyaschev <imz@altlinux.org>
2021-10-15 13:22:04 +0300

    squash! 1090371d9 Introduce RPMHandler::HasFile() and RPMHdrHandler::FileList()
    [imz: rpmhandler.cc: place HasFile() and FileList() definitions
    according to the order in apt-rpm upstream. Compared with:
    
    c613bd19e Mass constify rpmhandler getter methods
    
    ]

--- apt-pkg/rpm/rpmhandler.cc
+++ apt-pkg/rpm/rpmhandler.cc
@@ -35,17 +35,6 @@
 #include <rpm/rpmdb.h>
 #include <rpm/rpmlib.h>
 
-bool RPMHandler::HasFile(const char *File) const
-{
-   if (*File == '\0')
-      return false;
-
-   vector<string> Files;
-   FileList(Files);
-   vector<string>::const_iterator I = std::find(Files.begin(), Files.end(), File);
-   return I != Files.end();
-}
-
 string RPMHdrHandler::EVRDB() const
 {
    string str;
@@ -94,6 +83,17 @@ unsigned int RPMHandler::DepOp(raptDepFlags rpmflags) const
    }
 
    return Op;
+}
+
+bool RPMHandler::HasFile(const char *File) const
+{
+   if (*File == '\0')
+      return false;
+
+   vector<string> Files;
+   FileList(Files);
+   vector<string>::const_iterator I = std::find(Files.begin(), Files.end(), File);
+   return I != Files.end();
 }
 
 bool RPMHandler::InternalDep(const char *name, const char *ver, raptDepFlags flag) const
@@ -194,15 +194,6 @@ string RPMHdrHandler::Changelog() const
    return str;
 }
 
-bool RPMHdrHandler::FileList(vector<string> &FileList) const
-{
-   raptHeader h(HeaderP);
-   if (!h.getTag(RPMTAG_OLDFILENAMES, FileList))
-      h.getTag(RPMTAG_FILENAMES, FileList);
-   // it's ok for a package not to have any files
-   return true;
-}
-
 bool RPMHdrHandler::PRCO(unsigned int Type, vector<Dependency*> &Deps,
 			     bool checkInternalDep) const
 {
@@ -234,6 +225,15 @@ bool RPMHdrHandler::PRCO(unsigned int Type, vector<Dependency*> &Deps,
 		checkInternalDep, Deps);
       rpmdsFree(ds);
    }
+   return true;
+}
+
+bool RPMHdrHandler::FileList(vector<string> &FileList) const
+{
+   raptHeader h(HeaderP);
+   if (!h.getTag(RPMTAG_OLDFILENAMES, FileList))
+      h.getTag(RPMTAG_FILENAMES, FileList);
+   // it's ok for a package not to have any files
    return true;
 }
 
