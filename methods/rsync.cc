5655670d6fe20f9a2e84e6483e2a382c5353cf9e
Ivan Zakharyaschev <imz@altlinux.org>
2021-10-22 21:47:49 +0300

    methods/rsync.cc: use generalized Hashes class instead of MD5Summation

--- methods/rsync.cc
+++ methods/rsync.cc
@@ -12,7 +12,7 @@ RSYNC Aquire Method - This is the RSYNC aquire method for APT.
 #include <apt-pkg/fileutl.h>
 #include <apt-pkg/acquire-method.h>
 #include <apt-pkg/error.h>
-#include <apt-pkg/md5.h>
+#include <apt-pkg/hashes.h>
 
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -549,9 +549,9 @@ bool RsyncMethod::Fetch(FetchItem *Itm)
 		 //
 		 int fd = open(Itm->DestFile.c_str(), O_RDONLY);
 		 if (fd>=0) {
-			MD5Summation md5;
-			md5.AddFD(fd,st.st_size);
-			Res.MD5Sum = md5.Result();
+			Hashes Hash;
+			Hash.AddFD(fd,st.st_size);
+			Res.TakeHashes(Hash);
 			close(fd);
 		 }
 	  }
