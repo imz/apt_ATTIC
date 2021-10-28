imz/apt-rpm_next

- temporary hacks for gz instead of bz2 compression - almost does something :P

apt-pkg/init.cc
@@ -77,7 +77,7 @@ bool pkgInitConfig(Configuration &Cnf)
    Cnf.Set("Dir::Etc::parts","apt.conf.d");
    Cnf.Set("Dir::Etc::preferences","preferences");
    Cnf.Set("Dir::Bin::methods",LIBDIR "/apt/methods");
-   Cnf.Set("Acquire::ComprExtension", ".bz2");
+   Cnf.Set("Acquire::ComprExtension", ".gz");
 	      
    bool Res = true;
    

imz/apt_ATTIC

(cherry picked from commit 954587e879c22e085b3a8964ab0bc39668468ba6)
[imz: For the reasons described in the comment in the previous commit,
the file method would fail to find even some existing uncompressed
files in basedir if it assumes a wrong compression extension in the
queued item. This was demonstrated by our tests. Therefore, I modify
that place in methods/file.cc, too, to pass the tests.]

apt-pkg/init.cc
@@ -87,7 +87,7 @@ bool pkgInitConfig(Configuration &Cnf)
    Cnf.CndSet("Dir::Etc::preferences","preferences");
    Cnf.CndSet("Dir::Etc::preferencesparts","preferences.d");
    Cnf.CndSet("Dir::Bin::methods",LIBDIR "/apt/methods");
-   Cnf.CndSet("Acquire::ComprExtension", ".bz2");
+   Cnf.CndSet("Acquire::ComprExtension", ".gz");
 
    Cnf.Set("Dir::Ignore-Files-Silently::", "~$");
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.disabled$");
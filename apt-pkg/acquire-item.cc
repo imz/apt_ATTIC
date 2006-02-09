83cc4bd60caa8bbb88549d71445bf733ac83a39c
pmatilai <pmatilai>
2006-02-09 20:51:01 +0000

    - temporary hacks for gz instead of bz2 compression - almost does something :P
    (cherry picked from commit 954587e879c22e085b3a8964ab0bc39668468ba6)
    
    [imz: For the reasons described in the comment in the previous commit,
    the file method would fail to find even some existing uncompressed
    files in basedir if it assumes a wrong compression extension in the
    queued item. This was demonstrated by our tests. Therefore, I modify
    that place in methods/file.cc, too, to pass the tests.]

--- apt-pkg/acquire-item.cc
+++ apt-pkg/acquire-item.cc
@@ -198,7 +198,7 @@ pkgAcqIndex::pkgAcqIndex(pkgAcquire *Owner,pkgRepository *Repository,
 
    // Create the item
    // CNC:2002-07-03
-   Desc.URI = URI + _config->Find("Acquire::ComprExtension", ".bz2");
+   Desc.URI = URI + _config->Find("Acquire::ComprExtension", ".gz");
    Desc.Description = URIDesc;
    Desc.Owner = this;
    Desc.ShortDesc = ShortDesc;
@@ -368,10 +368,10 @@ void pkgAcqIndex::Done(string Message,unsigned long Size,string MD5,
    Decompression = true;
    DestFile += ".decomp";
    // CNC:2002-07-03
-   Desc.URI = "bzip2:" + FileName;
+   Desc.URI = "gzip:" + FileName;
    QueueURI(Desc);
    // CNC:2002-07-03
-   Mode = "bzip2";
+   Mode = "gzip";
 }
 									/*}}}*/
 
--- apt-pkg/init.cc
+++ apt-pkg/init.cc
@@ -87,7 +87,7 @@ bool pkgInitConfig(Configuration &Cnf)
    Cnf.CndSet("Dir::Etc::preferences","preferences");
    Cnf.CndSet("Dir::Etc::preferencesparts","preferences.d");
    Cnf.CndSet("Dir::Bin::methods",LIBDIR "/apt/methods");
-   Cnf.CndSet("Acquire::ComprExtension", ".bz2");
+   Cnf.CndSet("Acquire::ComprExtension", ".gz");
 
    Cnf.Set("Dir::Ignore-Files-Silently::", "~$");
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.disabled$");
