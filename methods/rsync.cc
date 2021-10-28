41794b07aee67e60a40ab1be48523248e8b2f4ca
Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>
2021-10-28 16:07:00 +0300

    methods/rsync: use class Hashes instead of counting MD5 hash

--- methods/rsync.cc
+++ methods/rsync.cc
@@ -545,7 +545,7 @@ bool RsyncMethod::Fetch(FetchItem *Itm)
    if ( server->Get(this,Res,File,Itm->DestFile.c_str()) ) {
 	  if ( stat(Itm->DestFile.c_str(), &st)==0 ) {
 		 Res.Size = st.st_size;
-		 // Calculating MD5
+		 // Calculating checksum
 		 //
 		 int fd = open(Itm->DestFile.c_str(), O_RDONLY);
 		 if (fd>=0) {
