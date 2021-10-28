hypnotux/apt

Eliminate uses of "using namespace std" from headers

apt-pkg/rpm/rpmlistparser.h
@@ -27,7 +27,9 @@
 #include <vector>
 #include <regex.h>
 
-using namespace std;
+using std::map;
+using std::vector;
+using std::string;
 
 class RPMHandler;
 class RPMPackageData;

imz/apt_ATTIC

[imz: adapt to the use of std::unordered_map in rpmpackagedata.h,
which was introduced later than this cherry-picked commit in
apt-rpm upstream. And to std::unordered_set in rpmlistparser.h.]
(Originally 07adc5b031736f758fe45798176115be09b798c3 at http://apt-rpm.org/scm/apt.git )
(Originally e403b7b26eaefeba8b0fc34bd011fb834723a4fa at https://gitlab.com/apt-rpm/apt-rpm.git )
A commit selected with:
git log --oneline --reverse 0.5.15sisyphus1/apt-repomd..0.5.15sisyphus1/apt-rpm --regexp-ignore-case -G'hash|md5|sha|cksum|checksum|chktype'

apt-pkg/rpm/rpmlistparser.h
@@ -22,7 +22,9 @@
 #include <regex.h>
 #include <optional>
 
-using namespace std;
+using std::unordered_set;
+using std::vector;
+using std::string;
 
 class RPMHandler;
 class RPMPackageData;