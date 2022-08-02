899933377ad88b77a447311feb8697e047686940
Ivan Zakharyaschev <imz@altlinux.org>
2022-08-02 20:01:56 +0300

    MarkInstallRec(): We care only about conflicting packages that are to be installed

--- apt-pkg/depcache.cc
+++ apt-pkg/depcache.cc
@@ -1137,7 +1137,17 @@ void pkgDepCache::MarkInstallRec(const PkgIterator &Pkg,
               I++)
 	 {
 	    VerIterator const TrgVer(*Cache,*I);
-            DBG.traceTraversal(1, "target to delete:", TrgVer);
+            const StateCache &TrgP = PkgState[TrgVer.ParentPkg()->ID];
+
+            // We care only about conflicting packages that are to be installed.
+            // If a conflicting package might get installed at other moments,
+            // that's not our responsibility to deal with the conflict now.
+            if (*I != TrgP.InstallVer)
+               continue;
+            DBG.traceTraversal(1, "found a target that is to be installed:", TrgVer);
+
+            DBG.traceTraversal(1, "resolving this conflict");
+
 	    PkgIterator const TrgPkg = TrgVer.ParentPkg();
 	    MarkDelete0(TrgPkg, false, DBG.deeper());
 	    MarkAuto(TrgPkg, getMarkAuto(TrgPkg));
